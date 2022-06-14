ffd45ad7906cff26fc99896f4bd9d9331c842a17
panduananto <pandunih@gmail.com>
2022-06-14 15:22:46 +0700

    init

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	plugins: [require('prettier-plugin-tailwindcss')],
+	tailwindConfig: './tailwind.config.js',
+	tabWidth: 2,
+	printWidth: 120,
+	useTabs: true,
+	singleQuote: true,
+};
