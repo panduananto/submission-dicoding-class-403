58aeaa6ff144287d7a9a201a5b9f32df2b555f1e
Pandu Ananto <pandunih@gmail.com>
2022-06-14 22:12:42 +0700

    navbar layout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 
+import SearchInput from './SearchInput';
+
 export default class NavBar extends Component {
 	render() {
 		return (
-			<nav className="flex items-center bg-purple-700 py-4 px-6">
-				<div className="flex items-center space-x-1">
-					<span>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="h-10 w-10 text-[#AC94FA]"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-								clip-rule="evenodd"
-							/>
-						</svg>
-					</span>
-					<span className="font-rubik text-xl font-medium text-white">Notenote</span>
+			<nav className="border-b border-slate-300 bg-white">
+				<div className="mx-auto flex max-w-screen-lg items-center py-4">
+					<div className="flex items-center space-x-2">
+						<span>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-10 w-10 text-lime-500"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</span>
+						<span className="font-rubik text-xl font-medium text-slate-900">Notenote</span>
+					</div>
+					<SearchInput></SearchInput>
 				</div>
 			</nav>
 		);
--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -2,6 +2,37 @@ import React, { Component } from 'react';
 
 export default class SearchInput extends Component {
 	render() {
-		return <div></div>;
+		return (
+			<form className="ml-auto block w-96">
+				<label htmlFor="search" className="sr-only mb-2 text-sm font-medium text-white ">
+					Search
+				</label>
+				<div className="relative">
+					<div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+						<svg
+							class="h-5 w-5 text-gray-500 dark:text-gray-400"
+							fill="none"
+							stroke="currentColor"
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+							></path>
+						</svg>
+					</div>
+					<input
+						type="text"
+						id="search"
+						autoComplete="off"
+						placeholder="Cari catatanmu di sini..."
+						className="block w-full rounded-lg border border-slate-300 bg-gray-50 p-3 pl-10 text-sm text-slate-900 placeholder:text-slate-400 focus:border-lime-500 focus:outline-none focus:ring-lime-500"
+					/>
+				</div>
+			</form>
+		);
 	}
 }
