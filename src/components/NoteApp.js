a9c5d47b229d8d86e470fa52a4c1069b76d26288
panduananto <pandunih@gmail.com>
2022-06-15 10:54:37 +0700

    creating note works

--- src/components/NoteApp.js
+++ src/components/NoteApp.js
@@ -3,10 +3,35 @@ import React, { Component } from 'react';
 import NoteForm from './NoteForm';
 
 export default class NoteApp extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			notes: [],
+		};
+
+		this.onAddNoteHandler = this.onAddNoteHandler.bind(this);
+	}
+
+	onAddNoteHandler({ title, content }) {
+		this.setState((prevState) => {
+			return {
+				notes: [
+					...prevState.notes,
+					{
+						id: +new Date(),
+						title,
+						content,
+					},
+				],
+			};
+		});
+	}
+
 	render() {
 		return (
 			<div className="mx-auto max-w-screen-lg py-12">
-				<NoteForm></NoteForm>
+				<NoteForm addNote={this.onAddNoteHandler}></NoteForm>
 			</div>
 		);
 	}
--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,27 +1,70 @@
 import React, { Component } from 'react';
 
 export default class NoteForm extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			title: '',
+			content: '',
+		};
+
+		this.onTitleChangeHandler = this.onTitleChangeHandler.bind(this);
+		this.onContentChangeHandler = this.onContentChangeHandler.bind(this);
+		this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
+	}
+
+	onTitleChangeHandler(event) {
+		this.setState((prevState) => {
+			return {
+				...prevState,
+				title: event.target.value,
+			};
+		});
+	}
+
+	onContentChangeHandler(event) {
+		this.setState((prevState) => {
+			return {
+				...prevState,
+				content: event.target.value,
+			};
+		});
+	}
+
+	onSubmitEventHandler(event) {
+		event.preventDefault();
+		this.props.addNote(this.state);
+	}
+
 	render() {
 		return (
-			<form>
-				<div className="w-[600px] overflow-hidden rounded-lg border border-slate-300 bg-white">
+			<form onSubmit={this.onSubmitEventHandler}>
+				<div className="w-[600px] overflow-hidden rounded-lg border border-slate-300 bg-white shadow-sm">
 					<div className="p-5">
-						<label htmlFor="note-title" className="sr-only mb-2 text-sm font-medium text-slate-700">
+						<label htmlFor="title" className="sr-only mb-2 text-sm font-medium text-slate-700">
 							Judul Catatan
 						</label>
 						<input
 							type="text"
-							id="note-title"
+							id="title"
 							autoComplete="off"
 							placeholder="Judul catatan"
+							value={this.state.title}
+							onChange={this.onTitleChangeHandler}
 							className="mb-2 block w-full border-0 p-0 font-semibold text-slate-900 outline-none placeholder:text-lg placeholder:font-semibold placeholder:text-slate-600 focus:border-0 focus:outline-none focus:ring-0"
 						/>
+						<label htmlFor="content" className="sr-only mb-2 text-sm font-medium text-slate-700">
+							Catatan
+						</label>
 						<textarea
-							name="note-content"
-							id="note-content"
+							name="content"
+							id="content"
 							cols="20"
 							rows="4"
 							placeholder="Tulis catatanmu di sini..."
+							value={this.state.content}
+							onChange={this.onContentChangeHandler}
 							className="block w-full resize-none border-0 p-0 text-slate-900 outline-none scrollbar-hide placeholder:font-medium placeholder:text-slate-600 focus:border-0 focus:outline-none focus:ring-0"
 						></textarea>
 					</div>
