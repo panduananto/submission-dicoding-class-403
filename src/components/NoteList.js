45b5f6311b2f20c3fa210eb1e1241a6dd66494c6
Pandu Ananto <pandunih@gmail.com>
2022-06-19 18:07:26 +0700

    render if notes empty

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -9,16 +9,28 @@ export default class NoteList extends Component {
 			<Fragment>
 				<Tabs>
 					{Object.keys(this.props.notes).map((key) => (
-						<div key={key} data-label={key} className="grid w-full grid-cols-12 gap-4">
-							{this.props.notes[key].map((note) => (
-								<NoteItem
-									key={note.id}
-									note={note}
-									label={key}
-									moveNoteToAnotherCollection={this.props.moveNoteToAnotherCollection}
-									removeNoteFromCollection={this.props.removeNoteFromCollection}
-								></NoteItem>
-							))}
+						<div
+							key={key}
+							data-label={key}
+							className={`${
+								this.props.notes[key].length !== 0 ? 'grid w-full grid-cols-12 gap-4' : 'w-full max-w-[600px]'
+							}`}
+						>
+							{this.props.notes[key].length !== 0 ? (
+								this.props.notes[key].map((note) => (
+									<NoteItem
+										key={note.id}
+										note={note}
+										label={key}
+										moveNoteToAnotherCollection={this.props.moveNoteToAnotherCollection}
+										removeNoteFromCollection={this.props.removeNoteFromCollection}
+									></NoteItem>
+								))
+							) : (
+								<div className="w-full rounded-lg border border-slate-300 p-3 text-center shadow-sm">
+									<p>Belum ada catatan di sini</p>
+								</div>
+							)}
 						</div>
 					))}
 				</Tabs>
