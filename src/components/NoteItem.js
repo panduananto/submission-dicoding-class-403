801525fc395e19e588c7392e15ba8fec5d4ee7ba
Pandu Ananto <pandunih@gmail.com>
2022-06-19 14:38:20 +0700

    base layout note edit modal

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,11 +1,105 @@
 import React, { Component } from 'react';
 
+import ReactModal from 'react-modal';
+import { MdArchive, MdDelete, MdOutlineModeEdit } from 'react-icons/md';
+
 export default class NoteItem extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			showModal: false,
+		};
+
+		this.handleOpenModal = this.handleOpenModal.bind(this);
+		this.handleCloseModal = this.handleCloseModal.bind(this);
+	}
+
+	handleOpenModal(event) {
+		event.stopPropagation();
+		this.setState({ showModal: true });
+	}
+
+	handleCloseModal(event) {
+		event.stopPropagation();
+		this.setState({ showModal: false });
+	}
+
 	render() {
 		return (
-			<div className="col-span-3 overflow-hidden rounded-lg border border-slate-300 bg-white p-3 text-slate-900 shadow-sm transition-shadow duration-200 ease-in-out hover:shadow-lg">
+			<div
+				onClick={this.handleOpenModal}
+				className="group relative col-span-3 overflow-hidden rounded-lg border border-slate-300 bg-white p-3 text-slate-900 shadow-sm transition-shadow duration-200 ease-in-out hover:shadow-lg"
+			>
 				<h1 className="mb-2 font-semibold">{this.props.note.title}</h1>
 				<p>{this.props.note.content}</p>
+				<span className="absolute top-3 right-3">
+					<MdOutlineModeEdit className="h-6 w-6 text-white transition-colors duration-200 ease-in-out group-hover:text-slate-500"></MdOutlineModeEdit>
+				</span>
+				<ReactModal
+					isOpen={this.state.showModal}
+					onRequestClose={this.handleCloseModal}
+					shouldCloseOnOverlayClick={true}
+					contentLabel="modal for editing note"
+					closeTimeoutMS={150}
+					style={{
+						content: {
+							position: 'static',
+							inset: '0px',
+							borderColor: '#cbd5e1',
+							borderRadius: '0.5rem',
+							boxShadow: 'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+							width: '100%',
+							maxWidth: '600px',
+						},
+						overlay: {
+							display: 'flex',
+							justifyContent: 'center',
+							alignItems: 'center',
+						},
+					}}
+				>
+					<form className="h-full">
+						<label htmlFor="title" className="sr-only mb-2 text-sm font-medium text-slate-700">
+							Judul Catatan
+						</label>
+						<input
+							type="text"
+							id="title"
+							autoComplete="off"
+							placeholder="Judul catatan"
+							value={this.props.note.title}
+							onChange={this.onTitleChangeHandler}
+							className="mb-2 block w-full border-0 p-0 font-semibold text-slate-900 outline-none placeholder:text-lg placeholder:font-semibold placeholder:text-slate-500 focus:border-0 focus:outline-none focus:ring-0"
+						/>
+						<label htmlFor="content" className="sr-only mb-2 text-sm font-medium text-slate-700">
+							Catatan
+						</label>
+						<textarea
+							name="content"
+							id="content"
+							cols="20"
+							rows="6"
+							placeholder="Tulis catatanmu di sini..."
+							value={this.props.note.content}
+							onChange={this.onContentChangeHandler}
+							className="block w-full resize-none border-0 p-0 text-slate-900 outline-none scrollbar-hide placeholder:font-medium placeholder:text-slate-500 focus:border-0 focus:outline-none focus:ring-0"
+						></textarea>
+						<div className="mt-8 flex justify-between">
+							<div className="inline-flex gap-2">
+								<button className="group inline-flex items-center rounded-lg bg-slate-100 p-2.5 text-sm font-medium text-slate-900 transition-colors duration-200 ease-in-out hover:bg-slate-200">
+									<MdArchive className="h-5 w-5 text-slate-500 transition-colors duration-200 ease-in-out group-hover:text-slate-900"></MdArchive>
+								</button>
+								<button className="group inline-flex items-center rounded-lg bg-slate-100 p-2.5 text-sm font-medium text-slate-900 transition-colors duration-200 ease-in-out hover:bg-slate-200">
+									<MdDelete className="h-5 w-5 text-slate-500 transition-colors duration-200 ease-in-out group-hover:text-slate-900"></MdDelete>
+								</button>
+							</div>
+							<button className="inline-flex items-center rounded-lg bg-slate-100 px-4 py-2 text-sm font-semibold text-slate-900 transition-colors duration-200 ease-in-out hover:bg-slate-200">
+								Done
+							</button>
+						</div>
+					</form>
+				</ReactModal>
 			</div>
 		);
 	}
--- src/components/TabsButton.js
+++ src/components/TabsButton.js
@@ -13,30 +13,30 @@ export default class TabsButton extends Component {
 				{this.props.label === 'catatan' ? (
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
-						class="h-6 w-6"
+						className="h-6 w-6"
 						fill="none"
 						viewBox="0 0 24 24"
 						stroke="currentColor"
-						stroke-width="2"
+						strokeWidth="2"
 					>
 						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
+							strokeLinecap="round"
+							strokeLinejoin="round"
 							d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
 						/>
 					</svg>
 				) : (
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
-						class="h-6 w-6"
+						className="h-6 w-6"
 						fill="none"
 						viewBox="0 0 24 24"
 						stroke="currentColor"
-						stroke-width="2"
+						strokeWidth="2"
 					>
 						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
+							strokeLinecap="round"
+							strokeLinejoin="round"
 							d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
 						/>
 					</svg>
